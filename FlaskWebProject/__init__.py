ae846a3c34b803d5de1ea2cedd7c581a05ff7b11
Darkmoon92 <88928955+Darkmoon92@users.noreply.github.com>
2023-04-07 16:25:16 +0000

    update new

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,16 +10,12 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# Add any logging levels and handlers with app.logger
-# Set the logging level
-app.logger.setLevel(logging.INFO)
-# Create a file handler
-handler = logging.FileHandler('app.log')
-# Set the logging format
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-# Add the handler to the logger
-app.logger.addHandler(handler)
+# TODO: Add any logging levels and handlers with app.logger
+app.logger.setLevel(logging.WARNING)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.WARNING)
+app.logger.addHandler(streamHandler) # add stream handler to app.logger object
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
